2c69e096188920edce26b59e30d3fc63e74a77ff
azaeng <aa22548659@gmail.com>
2025-04-08 15:35:38 +0900

    애니메이터 추가, 보드를 프리펩으로 변경
    플레이어의 걷기 애니메이터 추가
    보드를 모델에서 프리펩으로 변경

--- Assets/Photon/PhotonUnityNetworking/Demos/Shared Assets/Scripts/ThirdPersonCharacter.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/Shared Assets/Scripts/ThirdPersonCharacter.cs
@@ -2,224 +2,202 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson.PunDemos
 {
-	[RequireComponent(typeof(Rigidbody))]
-	[RequireComponent(typeof(CapsuleCollider))]
-	[RequireComponent(typeof(Animator))]
-	public class ThirdPersonCharacter : MonoBehaviour
-	{
-		[SerializeField] float m_MovingTurnSpeed = 360;
-		[SerializeField] float m_StationaryTurnSpeed = 180;
-		[SerializeField] float m_JumpPower = 12f;
-		[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
-		[SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
-		[SerializeField] float m_MoveSpeedMultiplier = 1f;
-		[SerializeField] float m_AnimSpeedMultiplier = 1f;
-		[SerializeField] float m_GroundCheckDistance = 0.1f;
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CapsuleCollider))]
+    [RequireComponent(typeof(Animator))]
+    public class ThirdPersonCharacter : MonoBehaviour
+    {
+        [SerializeField] float m_MovingTurnSpeed = 360;         // 이동 중 회전 속도
+        [SerializeField] float m_StationaryTurnSpeed = 180;     // 정지 중 회전 속도
+        [SerializeField] float m_JumpPower = 12f;                // 점프 힘
+        [Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;  // 중력 배수
+        [SerializeField] float m_RunCycleLegOffset = 0.2f;       // 달리는 애니메이션의 다리 움직임 오프셋
+        [SerializeField] float m_MoveSpeedMultiplier = 1f;       // 이동 속도 배수
+        [SerializeField] float m_AnimSpeedMultiplier = 1f;       // 애니메이션 속도 배수
+        [SerializeField] float m_GroundCheckDistance = 0.1f;     // 땅 체크 거리
 
-		Rigidbody m_Rigidbody;
-		Animator m_Animator;
-		bool m_IsGrounded;
-		float m_OrigGroundCheckDistance;
-		const float k_Half = 0.5f;
-		float m_TurnAmount;
-		float m_ForwardAmount;
-		Vector3 m_GroundNormal;
-		float m_CapsuleHeight;
-		Vector3 m_CapsuleCenter;
-		CapsuleCollider m_Capsule;
-		bool m_Crouching;
+        Rigidbody m_Rigidbody;
+        Animator m_Animator;
+        bool m_IsGrounded;                // 땅에 닿았는지 여부
+        float m_OrigGroundCheckDistance;
+        const float k_Half = 0.5f;
+        float m_TurnAmount;
+        float m_ForwardAmount;
+        Vector3 m_GroundNormal;
+        float m_CapsuleHeight;
+        Vector3 m_CapsuleCenter;
+        CapsuleCollider m_Capsule;
+        bool m_Crouching;                 // 웅크리는 상태인지
 
+        void Start()
+        {
+            m_Animator = GetComponent<Animator>();
+            m_Rigidbody = GetComponent<Rigidbody>();
+            m_Capsule = GetComponent<CapsuleCollider>();
+            m_CapsuleHeight = m_Capsule.height;
+            m_CapsuleCenter = m_Capsule.center;
 
-		void Start()
-		{
-			m_Animator = GetComponent<Animator>();
-			m_Rigidbody = GetComponent<Rigidbody>();
-			m_Capsule = GetComponent<CapsuleCollider>();
-			m_CapsuleHeight = m_Capsule.height;
-			m_CapsuleCenter = m_Capsule.center;
+            // Rigidbody 회전을 고정
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            m_OrigGroundCheckDistance = m_GroundCheckDistance;
+        }
 
-			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-			m_OrigGroundCheckDistance = m_GroundCheckDistance;
-		}
+        public void Move(Vector3 move, bool crouch, bool jump)
+        {
+            if (move.magnitude > 1f) move.Normalize();
 
+            // 월드 좌표 기준의 방향을 로컬 좌표로 변환
+            move = transform.InverseTransformDirection(move);
+            CheckGroundStatus();
+            move = Vector3.ProjectOnPlane(move, m_GroundNormal);
+            m_TurnAmount = Mathf.Atan2(move.x, move.z);
+            m_ForwardAmount = move.z;
 
-		public void Move(Vector3 move, bool crouch, bool jump)
-		{
+            ApplyExtraTurnRotation();
 
-			// convert the world relative moveInput vector into a local-relative
-			// turn amount and forward amount required to head in the desired
-			// direction.
-			if (move.magnitude > 1f) move.Normalize();
-			move = transform.InverseTransformDirection(move);
-			CheckGroundStatus();
-			move = Vector3.ProjectOnPlane(move, m_GroundNormal);
-			m_TurnAmount = Mathf.Atan2(move.x, move.z);
-			m_ForwardAmount = move.z;
+            if (m_IsGrounded)
+            {
+                HandleGroundedMovement(crouch, jump);  // 지면 위 동작 처리
+            }
+            else
+            {
+                HandleAirborneMovement();              // 공중 동작 처리
+            }
 
-			ApplyExtraTurnRotation();
+            ScaleCapsuleForCrouching(crouch);          // 웅크림 크기 조정
+            PreventStandingInLowHeadroom();           // 좁은 공간에서 서는 것 방지
+            UpdateAnimator(move);                      // 애니메이터 업데이트
+        }
 
-			// control and velocity handling is different when grounded and airborne:
-			if (m_IsGrounded)
-			{
-				HandleGroundedMovement(crouch, jump);
-			}
-			else
-			{
-				HandleAirborneMovement();
-			}
+        void ScaleCapsuleForCrouching(bool crouch)
+        {
+            if (m_IsGrounded && crouch)
+            {
+                if (m_Crouching) return;
+                m_Capsule.height /= 2f;
+                m_Capsule.center /= 2f;
+                m_Crouching = true;
+            }
+            else
+            {
+                // 위에 장애물이 있는지 확인
+                Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+                float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
+                if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+                {
+                    m_Crouching = true;
+                    return;
+                }
+                m_Capsule.height = m_CapsuleHeight;
+                m_Capsule.center = m_CapsuleCenter;
+                m_Crouching = false;
+            }
+        }
 
-			ScaleCapsuleForCrouching(crouch);
-			PreventStandingInLowHeadroom();
+        void PreventStandingInLowHeadroom()
+        {
+            if (!m_Crouching)
+            {
+                Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+                float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
+                if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+                {
+                    m_Crouching = true;
+                }
+            }
+        }
 
-			// send input and other state parameters to the animator
-			UpdateAnimator(move);
-		}
+        void UpdateAnimator(Vector3 move)
+        {
+            // 애니메이터에 파라미터 전달
+            m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
+            m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
+            m_Animator.SetBool("Crouch", m_Crouching);
+            m_Animator.SetBool("OnGround", m_IsGrounded);
 
+            if (!m_IsGrounded)
+            {
+                m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
+            }
 
-		void ScaleCapsuleForCrouching(bool crouch)
-		{
-			if (m_IsGrounded && crouch)
-			{
-				if (m_Crouching) return;
-				m_Capsule.height = m_Capsule.height / 2f;
-				m_Capsule.center = m_Capsule.center / 2f;
-				m_Crouching = true;
-			}
-			else
-			{
-				Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
-				float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
-				if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
-				{
-					m_Crouching = true;
-					return;
-				}
-				m_Capsule.height = m_CapsuleHeight;
-				m_Capsule.center = m_CapsuleCenter;
-				m_Crouching = false;
-			}
-		}
+            // 점프 다리 위치 계산
+            float runCycle = Mathf.Repeat(m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+            float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
+            if (m_IsGrounded)
+            {
+                m_Animator.SetFloat("JumpLeg", jumpLeg);
+            }
 
-		void PreventStandingInLowHeadroom()
-		{
-			// prevent standing up in crouch-only zones
-			if (!m_Crouching)
-			{
-				Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
-				float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
-				if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
-				{
-					m_Crouching = true;
-				}
-			}
-		}
+            // 애니메이션 속도 설정
+            if (m_IsGrounded && move.magnitude > 0)
+            {
+                m_Animator.speed = m_AnimSpeedMultiplier;
+            }
+            else
+            {
+                m_Animator.speed = 1;
+            }
+        }
 
+        void HandleAirborneMovement()
+        {
+            // 중력 보정 적용
+            Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
+            m_Rigidbody.AddForce(extraGravityForce);
 
-		void UpdateAnimator(Vector3 move)
-		{
-			// update the animator parameters
-			m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
-			m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
-			m_Animator.SetBool("Crouch", m_Crouching);
-			m_Animator.SetBool("OnGround", m_IsGrounded);
-			if (!m_IsGrounded)
-			{
-				m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
-			}
+            m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
+        }
 
-			// calculate which leg is behind, so as to leave that leg trailing in the jump animation
-			// (This code is reliant on the specific run cycle offset in our animations,
-			// and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
-			float runCycle =
-				Mathf.Repeat(
-					m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
-			float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
-			if (m_IsGrounded)
-			{
-				m_Animator.SetFloat("JumpLeg", jumpLeg);
-			}
+        void HandleGroundedMovement(bool crouch, bool jump)
+        {
+            if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
+            {
+                // 점프 실행
+                m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
+                m_IsGrounded = false;
+                m_Animator.applyRootMotion = false;
+                m_GroundCheckDistance = 0.1f;
+            }
+        }
 
-			// the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
-			// which affects the movement speed because of the root motion.
-			if (m_IsGrounded && move.magnitude > 0)
-			{
-				m_Animator.speed = m_AnimSpeedMultiplier;
-			}
-			else
-			{
-				// don't use that while airborne
-				m_Animator.speed = 1;
-			}
-		}
+        void ApplyExtraTurnRotation()
+        {
+            // 캐릭터 회전 보정
+            float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+            transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+        }
 
+        public void OnAnimatorMove()
+        {
+            // 루트 모션을 커스터마이즈하여 이동 적용
+            if (m_IsGrounded && Time.deltaTime > 0)
+            {
+                Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
+                v.y = m_Rigidbody.velocity.y; // y축 속도 유지
+                m_Rigidbody.velocity = v;
+            }
+        }
 
-		void HandleAirborneMovement()
-		{
-			// apply extra gravity from multiplier:
-			Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
-			m_Rigidbody.AddForce(extraGravityForce);
-
-			m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
-		}
-
-
-		void HandleGroundedMovement(bool crouch, bool jump)
-		{
-			// check whether conditions are right to allow a jump:
-			if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
-			{
-				// jump!
-				m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
-				m_IsGrounded = false;
-				m_Animator.applyRootMotion = false;
-				m_GroundCheckDistance = 0.1f;
-			}
-		}
-
-		void ApplyExtraTurnRotation()
-		{
-			// help the character turn faster (this is in addition to root rotation in the animation)
-			float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
-			transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
-		}
-
-
-		public void OnAnimatorMove()
-		{
-			// we implement this function to override the default root motion.
-			// this allows us to modify the positional speed before it's applied.
-			if (m_IsGrounded && Time.deltaTime > 0)
-			{
-				Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
-
-				// we preserve the existing y part of the current velocity.
-				v.y = m_Rigidbody.velocity.y;
-				m_Rigidbody.velocity = v;
-			}
-		}
-
-
-		void CheckGroundStatus()
-		{
-			RaycastHit hitInfo;
+        void CheckGroundStatus()
+        {
+            RaycastHit hitInfo;
 #if UNITY_EDITOR
-			// helper to visualise the ground check ray in the scene view
-			Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+            // 씬 뷰에서 디버그용 선을 그림
+            Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
 #endif
-			// 0.1f is a small offset to start the ray from inside the character
-			// it is also good to note that the transform position in the sample assets is at the base of the character
-			if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
-			{
-				m_GroundNormal = hitInfo.normal;
-				m_IsGrounded = true;
-				m_Animator.applyRootMotion = true;
-			}
-			else
-			{
-				m_IsGrounded = false;
-				m_GroundNormal = Vector3.up;
-				m_Animator.applyRootMotion = false;
-			}
-		}
-	}
+            // 아래 방향으로 레이캐스트를 쏴서 땅에 닿았는지 체크
+            if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+            {
+                m_GroundNormal = hitInfo.normal;
+                m_IsGrounded = true;
+                m_Animator.applyRootMotion = true;
+            }
+            else
+            {
+                m_IsGrounded = false;
+                m_GroundNormal = Vector3.up;
+                m_Animator.applyRootMotion = false;
+            }
+        }
+    }
 }
--- Assets/Photon/PhotonUnityNetworking/Demos/Shared Assets/Scripts/ThirdPersonUserControl.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/Shared Assets/Scripts/ThirdPersonUserControl.cs
@@ -6,16 +6,15 @@ namespace UnityStandardAssets.Characters.ThirdPerson.PunDemos
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class ThirdPersonUserControl : MonoBehaviour
     {
-        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-        private Vector3 m_CamForward;             // The current forward direction of the camera
-        private Vector3 m_Move;
-        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+        private ThirdPersonCharacter m_Character; // 이 오브젝트에 연결된 ThirdPersonCharacter 참조
+        private Transform m_Cam;                  // 씬에서 메인 카메라의 Transform 참조
+        private Vector3 m_CamForward;             // 카메라의 현재 전방 방향
+        private Vector3 m_Move;                   // 이동 벡터
+        private bool m_Jump;                      // 점프 여부 (카메라 기준의 이동 방향을 계산해 사용자의 입력에 따라 결정됨)
 
-        
         private void Start()
         {
-            // get the transform of the main camera
+            // 메인 카메라의 Transform을 가져옴
             if (Camera.main != null)
             {
                 m_Cam = Camera.main.transform;
@@ -23,52 +22,52 @@ namespace UnityStandardAssets.Characters.ThirdPerson.PunDemos
             else
             {
                 Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+                    "경고: 메인 카메라가 없습니다. ThirdPersonCharacter는 \"MainCamera\" 태그가 설정된 카메라가 필요합니다. 없을 경우 카메라 기준 이동이 적용되지 않습니다.", gameObject);
+                // 이 경우, 캐릭터 기준의 상대적인 이동이 사용됩니다. 의도한 바는 아닐 수 있지만, 경고는 했습니다!
             }
 
-            // get the third person character ( this should never be null due to require component )
+            // ThirdPersonCharacter 컴포넌트를 가져옴 (RequireComponent 때문에 null이 될 수 없음)
             m_Character = GetComponent<ThirdPersonCharacter>();
         }
 
-
         private void Update()
         {
+            // 점프 키가 눌렸는지 확인 (한 번만 체크)
             if (!m_Jump)
             {
                 m_Jump = Input.GetButtonDown("Jump");
             }
         }
 
-
-        // Fixed update is called in sync with physics
+        // FixedUpdate는 물리 연산과 동기화되어 호출됨
         private void FixedUpdate()
         {
-            // read inputs
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+            // 입력 값 받기
+            float h = Input.GetAxis("Horizontal"); // 좌우 이동
+            float v = Input.GetAxis("Vertical");   // 앞뒤 이동
+            bool crouch = Input.GetKey(KeyCode.C); // C키로 앉기 여부 판단
 
-            // calculate move direction to pass to character
+            // 캐릭터에 전달할 이동 방향 계산
             if (m_Cam != null)
             {
-                // calculate camera relative direction to move:
+                // 카메라 기준의 상대적 이동 방향 계산
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+                m_Move = v * m_CamForward + h * m_Cam.right;
             }
             else
             {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
+                // 메인 카메라가 없을 경우, 월드 기준 방향 사용
+                m_Move = v * Vector3.forward + h * Vector3.right;
             }
+
 #if !MOBILE_INPUT
-			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+            // 쉬프트 키를 누르면 걷기 속도로 이동
+            if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
 #endif
 
-            // pass all parameters to the character control script
+            // 캐릭터 컨트롤 스크립트에 모든 파라미터 전달
             m_Character.Move(m_Move, crouch, m_Jump);
-            m_Jump = false;
+            m_Jump = false; // 점프 초기화
         }
     }
 }
