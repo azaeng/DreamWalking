af0badb57a86ced7b3f8b2434a6589ad6a871a9c
parksungsoo99 <tigeroad3@naver.com>
2025-04-25 13:28:05 +0900

    joinManager 코드 추가

--- Assets/Scripts/DBScripts/JoinManager.cs
+++ Assets/Scripts/DBScripts/JoinManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Text.RegularExpressions; // 정규식 사용을 위한 네임스페이스 추가
+
+using Firebase.Database;
+
 
 public class JoinManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private InputField idInputField;
+    [SerializeField] private InputField passwordInputField;
+    [SerializeField] InputField confirmPasswordInputField;
+    [SerializeField] Text warningText;
 
-    // Update is called once per frame
-    void Update()
+    public void Create()
     {
-        
+        FirebaseAuthManager.Instance.Init();
+
+        string id = idInputField.text;
+        string password = passwordInputField.text;
+        string confirmPassword = confirmPasswordInputField.text;
+
+        // 입력값 유효성 검사
+
     }
 }
